bef98d5bac188a83a0ae3e254751d7defec300d4
Gibriil <musicmandanceman@yahoo.com>
2020-03-31 13:34:31 -0600

    Seed script now populates DB

--- prisma/seed.js
+++ prisma/seed.js
@@ -13,24 +13,65 @@ import {
 
 const prismaClient = new PrismaClient();
 
+const AllWeapons = weapons.map(weapon => {
+    return {
+        data: {
+            name: weapon.name || "",
+            category: weapon.category || "",
+            type: weapon.type || "",
+            dice: weapon.dice || 0,
+            adds: weapon.adds || 0,
+            strengthReq: weapon.strengthReq || 0,
+            dexReq: weapon.dexReq || 0,
+            cost: weapon.cost || 0,
+            weight: weapon.weight || 0,
+            range: weapon.range || "",
+            length: weapon.length || "",
+            extraInfo: weapon.extraInfo || ""
+        }
+    };
+});
+
+const AllArmour = armour.map(armor => {
+    return {
+        data: {
+            name: armor.name || "",
+            category: armor.category || "",
+            type: armor.type || "",
+            hits: armor.hits || 0,
+            strengthReq: armor.strengthReq || 0,
+            cost: armor.cost || 0,
+            weight: armor.weight || 0,
+            extraInfo: armor.extraInfo || ""
+        }
+    };
+});
+
+const AllSpells = spells.map(spell => {
+    return {
+        data: {
+            name: spell.name || "",
+            level: spell.level || 0,
+            description: spell.description || "",
+            cost: spell.cost || "",
+            range: spell.range || "",
+            symbol: spell.symbol || ""
+        }
+    };
+});
+
 async function main() {
     try {
-        for (let bob of weapons) {
-            await prismaClient.weapon.create({
-                    data: bob
-                })
-                .catch(err => console.log(`Error trying to create weapon: ${err} weapon ${bob}`))
+        for (let weapon of AllWeapons) {
+            await prismaClient.weapon.create(weapon)
+                .catch(err => console.log(`Error trying to create weapon: ${err} weapon ${weapon}`))
         }
-        for (let armor of armour) {
-            await prismaClient.armour.create({
-                    data: armor
-                })
-                .catch(err => console.log(`Error trying to create weapon: ${err} armour ${armor}`))
+        for (let armour of AllArmour) {
+            await prismaClient.armour.create(armour)
+                .catch(err => console.log(`Error trying to create weapon: ${err} armour ${armour}`))
         }
-        for (let spell of spells) {
-            await prismaClient.spells.create({
-                    data: weapon
-                })
+        for (let spell of AllSpells) {
+            await prismaClient.spells.create(spell)
                 .catch(err => console.log(`Error trying to create weapon: ${err} spell ${spell}`))
         }
     } catch (err) {
