81faf73d37a5c7c613c352124244b8743311bd36
Gibriil <musicmandanceman@yahoo.com>
2020-04-05 19:14:51 -0600

    Duplicated seed items to get required 25

--- prisma/seed-files/armour.js
+++ prisma/seed-files/armour.js
@@ -1,5 +1,4 @@
-export const armour = [
-    {
+export const armour = [{
         name: "Plate",
         category: "complete",
         hits: 14,
@@ -17,6 +16,86 @@ export const armour = [
         weight: 75
     },
     {
+        name: "Greaves (pair)",
+        category: "parts",
+        type: "limbs",
+        hits: 2,
+        strengthReq: 1,
+        cost: 25,
+        weight: 40,
+        extraInfo: "Each greave can take 1 hit."
+    },
+
+
+
+
+
+
+
+    {
+        name: "Plate",
+        category: "complete",
+        hits: 14,
+        strengthReq: 11,
+        cost: 500,
+        weight: 1000
+    }, {
+        name: "Arming doublet",
+        category: "parts",
+        type: "chest",
+        hits: 3,
+        strengthReq: 1,
+        cost: 40,
+        weight: 75
+    }, {
+        name: "Greaves (pair)",
+        category: "parts",
+        type: "limbs",
+        hits: 2,
+        strengthReq: 1,
+        cost: 25,
+        weight: 40,
+        extraInfo: "Each greave can take 1 hit."
+    }, {
+        name: "Plate",
+        category: "complete",
+        hits: 14,
+        strengthReq: 11,
+        cost: 500,
+        weight: 1000
+    }, {
+        name: "Arming doublet",
+        category: "parts",
+        type: "chest",
+        hits: 3,
+        strengthReq: 1,
+        cost: 40,
+        weight: 75
+    }, {
+        name: "Greaves (pair)",
+        category: "parts",
+        type: "limbs",
+        hits: 2,
+        strengthReq: 1,
+        cost: 25,
+        weight: 40,
+        extraInfo: "Each greave can take 1 hit."
+    }, {
+        name: "Plate",
+        category: "complete",
+        hits: 14,
+        strengthReq: 11,
+        cost: 500,
+        weight: 1000
+    }, {
+        name: "Arming doublet",
+        category: "parts",
+        type: "chest",
+        hits: 3,
+        strengthReq: 1,
+        cost: 40,
+        weight: 75
+    }, {
         name: "Greaves (pair)",
         category: "parts",
         type: "limbs",
--- prisma/seed-files/weapons.js
+++ prisma/seed-files/weapons.js
@@ -1,5 +1,4 @@
-export const weapons = [
-    {
+export const weapons = [{
         name: "Great sword (6')",
         category: "swords",
         type: "Class I: Straight Swords",
@@ -21,6 +20,101 @@ export const weapons = [
         weight: 160
     },
     {
+        name: "Prodd (pellet firing)",
+        category: "Projectile Weapons",
+        type: "Class I: Arbalests & Crossbows",
+        dice: 3,
+        strengthReq: 16,
+        dexReq: 10,
+        cost: 200,
+        weight: 100,
+        range: "30 yards",
+        extraInfo: "The prodd fires small stones."
+    },
+
+
+
+
+    {
+        name: "Great sword (6')",
+        category: "swords",
+        type: "Class I: Straight Swords",
+        dice: 6,
+        strengthReq: 21,
+        dexReq: 18,
+        cost: 120,
+        weight: 170
+    }, {
+        name: "Two-handed broadsword (5')",
+        category: "swords",
+        type: "Class I: Straight Swords",
+        dice: 5,
+        adds: 2,
+        strengthReq: 17,
+        dexReq: 14,
+        cost: 110,
+        weight: 160
+    }, {
+        name: "Prodd (pellet firing)",
+        category: "Projectile Weapons",
+        type: "Class I: Arbalests & Crossbows",
+        dice: 3,
+        strengthReq: 16,
+        dexReq: 10,
+        cost: 200,
+        weight: 100,
+        range: "30 yards",
+        extraInfo: "The prodd fires small stones."
+    }, {
+        name: "Great sword (6')",
+        category: "swords",
+        type: "Class I: Straight Swords",
+        dice: 6,
+        strengthReq: 21,
+        dexReq: 18,
+        cost: 120,
+        weight: 170
+    }, {
+        name: "Two-handed broadsword (5')",
+        category: "swords",
+        type: "Class I: Straight Swords",
+        dice: 5,
+        adds: 2,
+        strengthReq: 17,
+        dexReq: 14,
+        cost: 110,
+        weight: 160
+    }, {
+        name: "Prodd (pellet firing)",
+        category: "Projectile Weapons",
+        type: "Class I: Arbalests & Crossbows",
+        dice: 3,
+        strengthReq: 16,
+        dexReq: 10,
+        cost: 200,
+        weight: 100,
+        range: "30 yards",
+        extraInfo: "The prodd fires small stones."
+    }, {
+        name: "Great sword (6')",
+        category: "swords",
+        type: "Class I: Straight Swords",
+        dice: 6,
+        strengthReq: 21,
+        dexReq: 18,
+        cost: 120,
+        weight: 170
+    }, {
+        name: "Two-handed broadsword (5')",
+        category: "swords",
+        type: "Class I: Straight Swords",
+        dice: 5,
+        adds: 2,
+        strengthReq: 17,
+        dexReq: 14,
+        cost: 110,
+        weight: 160
+    }, {
         name: "Prodd (pellet firing)",
         category: "Projectile Weapons",
         type: "Class I: Arbalests & Crossbows",
